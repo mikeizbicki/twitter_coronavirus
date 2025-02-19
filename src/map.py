c444d83f5927c2ca3bec0f9f71d12067cb468376
maxplush <maxabeplush@gmail.com>
2025-02-19 11:37:22 -0800

    updated mappyer

--- src/map.py
+++ src/map.py
@@ -37,12 +37,15 @@ hashtags = [
 
 # initialize counters
 counter_lang = defaultdict(lambda: Counter())
+counter_country = defaultdict(lambda: Counter())
 
 # open the zipfile
 with zipfile.ZipFile(args.input_path) as archive:
 
     # loop over every file within the zip file
     for i,filename in enumerate(archive.namelist()):
+        if i >= 10: #stop after 10
+            break
         print(datetime.datetime.now(),args.input_path,filename)
 
         # open the inner file
@@ -56,13 +59,19 @@ with zipfile.ZipFile(args.input_path) as archive:
 
                 # convert text to lower case
                 text = tweet['text'].lower()
-
-                # search hashtags
+                
+                # Extract country code safely
+                country = tweet.get('place', {}).get('country_code', 'unknown')
+               
+               # search hashtags
                 for hashtag in hashtags:
                     lang = tweet['lang']
                     if hashtag in text:
                         counter_lang[hashtag][lang] += 1
+                        counter_country[hashtag][country] += 1
                     counter_lang['_all'][lang] += 1
+                    counter_country['_all'][country] += 1
+
 
 # open the outputfile
 try:
@@ -75,4 +84,10 @@ output_path_lang = output_path_base+'.lang'
 print('saving',output_path_lang)
 with open(output_path_lang,'w') as f:
     f.write(json.dumps(counter_lang))
+
+output_path_country = output_path_base+'.country'
+print('saving',output_path_country)
+with open(output_path_country,'w') as f:
+    f.write(json.dumps(counter_country))
+
 
